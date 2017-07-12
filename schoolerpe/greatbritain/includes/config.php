691d8de4d8d73cb887755f1b333d23c9d5a41c1e
msrinivasangit <mohana.srinivasan@rentsher.com>
2017-07-12 14:24:31 +0530

    new proj commited

---
+++ schoolerpe/greatbritain/includes/config.php
@@ -0,0 +1,4 @@
+<?
+$connection=mysql_connect("localhost","root","admin");
+$db=mysql_select_db("schoolnumo_new",$connection);
+?>
---
+++ schoolerpe/includes/constants.inc.php
@@ -0,0 +1,7 @@
+<?php
+
+  define('DB_SERVER', 'localhost');
+  define('DB_SERVER_USERNAME', 'root');
+  define('DB_SERVER_PASSWORD', 'admin');
+  define('DB_DATABASE', 'schoolnumo_new');
+?>
